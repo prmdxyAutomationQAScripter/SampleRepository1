d1513e80bf7642d9746d2e27f5385de53123dc35
PK <PK@DESKTOP-HH43MSS>
2019-10-06 18:07:05 +0530

    Demo2 & Demo3 added

---
+++ ABCD_Project/src/pac1/Demo2.java
@@ -0,0 +1,10 @@
+package pac1;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("Demo2");
+		System.out.println("Demo2in3");
+		System.out.println("Demo2in3");
+	}
+
+}
---
+++ ABCD_Project/src/pac1/Demo3.java
@@ -0,0 +1,8 @@
+package pac1;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		System.out.println("Demo3");
+	}
+
+}
