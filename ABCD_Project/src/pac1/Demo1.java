1606d7e71f49a9c40c73155755a1a8ae67183f6c
PK <PK@DESKTOP-HH43MSS>
2019-10-06 17:47:07 +0530

    ARC1 and url added/updated

---
+++ ABCD_Project/src/pac1/Demo1.java
@@ -0,0 +1,10 @@
+package pac1;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("Demo1");
+		System.out.println("ARC1");
+		//https://github.com/prmdxyAutomationQAScripter/SampleRepository1.git
+	}
+
+}
