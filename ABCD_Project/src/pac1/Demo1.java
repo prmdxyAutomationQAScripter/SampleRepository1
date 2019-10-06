9739f7069c7bcb73aa52382ba699ca4ac4f73f71
PK <PK@DESKTOP-HH43MSS>
2019-10-06 18:00:29 +0530

    updated

--- ABCD_Project/src/pac1/Demo1.java
+++ ABCD_Project/src/pac1/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 		System.out.println("Demo1");
 		System.out.println("ARC1");
 		//https://github.com/prmdxyAutomationQAScripter/SampleRepository1.git
-		System.out.println("Demo1in3");
+		System.out.println("Demo1in3"); 
 	}
 
 }
